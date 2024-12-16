22d905b407c37e88ef17d61425e51049a24059ac
Ketan5050 <khuntiketan50@gmail.com>
2024-12-16 21:31:47 +0530

    Created bicycle class

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,3 +1,25 @@
 public class Bicycle {
+    int speed=0;
+    int cadence=0;
+    int gear=0;
+
+    void changeCadence(int cadence){
+        this.cadence=cadence;
+    }
+    void changeGear(int gear){
+        this.gear=gear;
+    }
+    void speedUp(int speed){
+        this.speed+=speed;
+    }
+    void applyBrakes(int speed){
+        this.speed-=speed;
+    }
+    void printStates() {
+        System.out.println("cadence:" +
+                cadence + " speed:" +
+                speed + " gear:" + gear);
+    }
 
 }
+
